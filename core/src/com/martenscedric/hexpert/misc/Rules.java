942e0ee28f60ab79b7ac35516f48752de4b5d5b7
MartensCedric <cedricmartens98@gmail.com>
2017-06-02 17:16:24 -0400

    Chemo-ed some shit

--- core/src/com/martenscedric/hexpert/misc/Rules.java
+++ core/src/com/martenscedric/hexpert/misc/Rules.java
@@ -4,6 +4,7 @@ import com.cedricmartens.hexmap.hexagon.Hexagon;
 import com.martenscedric.hexpert.tile.BuildingType;
 import com.martenscedric.hexpert.tile.TileData;
 
+import java.util.HashMap;
 import java.util.List;
 
 import static com.martenscedric.hexpert.tile.BuildingType.ROCKET;
@@ -97,73 +98,36 @@ public class Rules
     {
         if(data.getBuildingType() != BuildingType.NONE)
             return false;
+
         List<Hexagon<TileData>> neighbors = data.getParent().getNeighbors();
 
-        boolean farm = false;
-        boolean house = false;
-        boolean mine = false;
-        boolean wind = false;
-        boolean factory = false;
-        boolean market = false;
-        boolean bank = false;
-        boolean rocket = false;
+        int[] neighborStats = new int[Const.BUILDING_COUNT];
 
-        for(int i = 0; i < neighbors.size(); i++)
-        {
+        for(int i = 0; i < neighbors.size(); i++) {
             TileData tileData = neighbors.get(i).getHexData();
 
-            switch (tileData.getBuildingType()) {
-                case NONE:
-                    break;
-                case FARM:
-                    farm = true;
-                    break;
-                case HOUSE:
-                    house = true;
-                    break;
-                case MINE:
-                    mine = true;
-                    break;
-                case WIND:
-                    wind = true;
-                    break;
-                case FACTORY:
-                    factory = true;
-                    break;
-                case MARKET:
-                    market = true;
-                    break;
-                case BANK:
-                    bank = true;
-                    break;
-                case ROCKET:
-                    rocket = true;
-                    break;
+            if(tileData.getBuildingType() != BuildingType.NONE)
+            {
+                neighborStats[tileData.getBuildingType().ordinal() - 1]++;
             }
         }
 
-        switch (selection) {
-            case NONE:
+        for(int i = 0; i < selection.getDenied().length; i++)
+        {
+            if(selection.getDenied()[i] != 0
+                    && selection.getDenied()[i] <= neighborStats[i])
+            {
                 return false;
-            case FARM:
-                return !farm;
-            case HOUSE:
-                return farm;
-            case MINE:
-                return house;
-            case WIND:
-                return house;
-            case FACTORY:
-                return house && mine && wind;
-            case MARKET:
-                return house && wind && factory;
-            case BANK:
-                return house && wind && mine && market;
-            case ROCKET:
-                return house && bank && factory && wind;
+            }
         }
 
-        return false;
+        for(int i = 0; i < selection.getRequired().length; i++)
+        {
+            if(neighborStats[i] < selection.getRequired()[i])
+                return false;
+        }
+
+        return true;
     }
 
     private static int neighborCountOf(BuildingType buildingType, TileData data)
--- core/src/com/martenscedric/hexpert/tile/BuildingType.java
+++ core/src/com/martenscedric/hexpert/tile/BuildingType.java
@@ -5,23 +5,28 @@ package com.martenscedric.hexpert.tile;
  */
 public enum  BuildingType
 {
-    NONE("None", 0),
-    FARM("Farm", -1),
-    HOUSE("House", -1),
-    MINE("Mine", 1),
-    WIND("Wind turbine", 1),
-    FACTORY("Factory", 2),
-    MARKET("Market", 3),
-    BANK("Bank", 5),
-    ROCKET("Rocket", 8);
+    NONE("None", 0, new int[]{}, new int[]{}),
+    FARM("Farm", -1, new int[]{}, new int[]{1, 0, 0, 0, 0, 0, 0, 0}),
+    HOUSE("House", -1, new int[]{1, 0, 0, 0, 0, 0, 0, 0}, new int[]{}),
+    MINE("Mine", 1, new int[]{0, 1, 0, 0, 0, 0, 0, 0}, new int[]{}),
+    WIND("Wind turbine", 1, new int[]{0, 1, 0, 0, 0, 0, 0, 0}, new int[]{}),
+    FACTORY("Factory", 2, new int[]{0, 1, 1, 1, 0, 0, 0, 0}, new int[]{}),
+    MARKET("Market", 3, new int[]{0, 1, 0, 1, 1, 0, 0, 0}, new int[]{}),
+    BANK("Bank", 5, new int[]{0, 1, 1, 1, 0, 1, 0, 0}, new int[]{}),
+    ROCKET("Rocket", 8, new int[]{0, 1, 0, 1, 1, 0, 1, 0}, new int[]{});
+
 
     private String name;
     private int score;
+    private int[] required;
+    private int[] denied;
 
-    BuildingType(String name, int score)
+    BuildingType(String name, int score, int[] required, int[] denied)
     {
         this.name = name;
         this.score = score;
+        this.required = required;
+        this.denied = denied;
     }
 
     public String getName() {
@@ -30,5 +35,13 @@ public enum  BuildingType
 
     public int getScore() {
         return score;
+    }
+
+    public int[] getRequired() {
+        return required;
+    }
+
+    public int[] getDenied() {
+        return denied;
     }
 }
