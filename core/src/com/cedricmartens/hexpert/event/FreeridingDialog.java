5121e0f40e33786518817d801482f6fe899f9ed1
MartensCedric <cedricmartens98@gmail.com>
2017-07-01 15:01:05 -0400

    Added FreeridingDialog

---
+++ core/src/com/cedricmartens/hexpert/event/FreeridingDialog.java
@@ -0,0 +1,46 @@
+package com.cedricmartens.hexpert.event;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.I18NBundle;
+import com.cedricmartens.hexpert.Hexpert;
+
+/**
+ * Created by martens on 7/1/17.
+ */
+
+public class FreeridingDialog extends StandardDialog {
+    public FreeridingDialog(Hexpert hexpert, Skin skin)
+    {
+        super(hexpert, skin.get("gold", WindowStyle.class));
+
+        getBackground().setMinWidth(1400);
+        I18NBundle i18N = hexpert.i18NBundle;
+        Label lblRate = new Label(i18N.get("freerider"), skin.get("bigger", Label.LabelStyle.class));
+        lblRate.setAlignment(Align.center);
+        lblRate.setWrap(true);
+        getContentTable().add(lblRate).width(1200);
+
+        TextButton.TextButtonStyle goldenStyle = skin.get("gold", TextButton.TextButtonStyle.class);
+
+        TextButton textButtonNo = new TextButton(i18N.get("no"), goldenStyle);
+        getButtonTable().add(textButtonNo);
+        setObject(textButtonNo, null);
+
+        TextButton textButtonRate = new TextButton(i18N.get("rate"), goldenStyle);
+        getButtonTable().add(textButtonRate);
+        setObject(textButtonRate, null);
+    }
+
+    @Override
+    protected void result(Object object) {
+        super.result(object);
+    }
+
+    @Override
+    public float getPrefHeight() {
+        return 600;
+    }
+}
--- core/src/com/cedricmartens/hexpert/event/PurchaseDialog.java
+++ core/src/com/cedricmartens/hexpert/event/PurchaseDialog.java
@@ -1,13 +1,17 @@
 package com.cedricmartens.hexpert.event;
 
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Slider;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.I18NBundle;
 import com.cedricmartens.hexpert.Hexpert;
+import com.cedricmartens.hexpert.social.Purchasing;
 
 /**
  * Created by martens on 7/1/17.
@@ -17,15 +21,23 @@ public class PurchaseDialog extends StandardDialog {
 
     private Label lblPrice;
 
-    public PurchaseDialog(Hexpert hexpert, Skin skin) {
-        super(hexpert, skin);
+    public PurchaseDialog(final Hexpert hexpert, final Skin skin) {
+        super(hexpert, skin.get("gold", WindowStyle.class));
 
-        
-        int defaultPrice = 2;
-        I18NBundle i18N = hexpert.i18NBundle;
-        lblPrice = new Label(String.format("$%d", defaultPrice), skin.get("bigger", Label.LabelStyle.class));
         getBackground().setMinWidth(1600);
-        getContentTable().add(lblPrice);
+
+        I18NBundle i18N = hexpert.i18NBundle;
+        Label.LabelStyle lblStyleBigger = skin.get("bigger", Label.LabelStyle.class);
+
+        Label lblExplaination = new Label(i18N.get("pwyw"), lblStyleBigger);
+        lblExplaination.setWrap(true);
+        lblExplaination.setAlignment(Align.center);
+        getContentTable().add(lblExplaination).width(1400);
+        getContentTable().row();
+        int defaultPrice = 2;
+        lblPrice = new Label(String.format("$%d", defaultPrice), lblStyleBigger);
+
+        getContentTable().add(lblPrice).padTop(150);
         getContentTable().row();
         final Slider slider = new Slider(0, 10, 1, false, skin);
         slider.setValue(defaultPrice);
@@ -35,13 +47,25 @@ public class PurchaseDialog extends StandardDialog {
                 lblPrice.setText(String.format("$%d", (int)slider.getValue()));
             }
         });
-        slider.setDebug(true);
-        getContentTable().add(slider).width(1200).height(200);
-        getContentTable().setDebug(true);
-        getButtonTable().defaults().pad(15);
 
-        TextButton textButtonOk = new TextButton(i18N.get("ok"), skin);
+        getContentTable().add(slider).width(1200).height(100);
+
+        TextButton textButtonOk = new TextButton(i18N.get("ok"), skin.get("gold", TextButton.TextButtonStyle.class));
         getButtonTable().add(textButtonOk);
+        textButtonOk.addListener(new ClickListener()
+        {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                Purchasing.Amount amount = Purchasing.Amount.values()[(int)slider.getValue()];
+
+                if(amount == Purchasing.Amount.ZERO)
+                {
+                   new FreeridingDialog(hexpert, skin).show(getStage());
+                }
+
+                hide();
+            }
+        });
 
         setObject(textButtonOk, null);
     }
